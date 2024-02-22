38e31c75b58a38e89b59ce28d673b4bedda6f07e
Aakash T <aakashvnth@gmail.com>
2024-02-22 15:42:54 +0530

    Initial Commit

---
+++ src/main/java/com/cyberaakash/controlleddatafetch/ControlledDataFetchApplication.java
@@ -0,0 +1,13 @@
+package com.cyberaakash.controlleddatafetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlledDataFetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlledDataFetchApplication.class, args);
+	}
+
+}
